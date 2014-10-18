1e0a64ecc0a152e7eab130de8dc00f41715df093
Marcelo Brito <marcelobritowd@hotmail.com>
2014-10-18 02:05:13 -0300

    desenvilvimento caminhando, vamo que vamo

--- Gruntfile.js
+++ Gruntfile.js
@@ -13,10 +13,30 @@ module.exports = function (grunt) {
 		  }
 		}
 
+
+		'string-replace': {
+		  inline: {
+		    files: {
+		      'css/': 'css/**',
+		    },
+		    options: {
+		      replacements: [
+		        // place files inline example
+		        {
+		          pattern: '<link rel="stylesheet" href="css/style.css">',
+		          replacement: '<link rel="stylesheet" <%= grunt.file.read('css/style.min.css') %>'
+		        }
+		      ]
+		    }
+		  }
+		}
+
+
+
 	});
 
-	grunt.loadNpmTasks('grunt-contrib-cssmin');
-	grunt.registerTask('default', ['cssmin']);
+	grunt.loadNpmTasks('grunt-contrib-cssmin', 'grunt-string-replace');
+	grunt.registerTask('default', ['cssmin', 'string-replace']);
 
 
 };
