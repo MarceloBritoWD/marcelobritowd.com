0aca86fa4cf658d3737e43c386924ae7eb815fd3
MarceloBrito <marcelobritowd@hotmail.com>
2015-03-31 00:52:12 -0300

    todo o css passado para stylus e modularizado para facilitar as ediçoes

---
+++ js/functions.js
@@ -0,0 +1,4 @@
+	// random color
+	var color = ['#4682b4', '#b22222', '#503583', '#000000', '#FF4500', '#708090', '#8B4513', '#556B2F', '#4C2A36', '#290C2E'];
+	var rand = color[Math.floor(Math.random() * color.length)];
+	document.getElementById("geral").style.background = rand;
