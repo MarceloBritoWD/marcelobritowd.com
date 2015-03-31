3574236bae370775d477ae5b4f510007ea521733
MarceloBrito <marcelobritowd@hotmail.com>
2015-03-31 20:11:53 -0300

    site pronto e todo passado pra stylus e todo modularizado, tanto o css como o js

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,6 +8,14 @@ module.exports = function (grunt) {
 	      files: ['src/styl/*.styl'],
 	      tasks: ['stylus'],
 	    },
+
+	    scripts: {
+		    files: ['src/js/*.js'],
+		    tasks: ['uglify'],
+		    options: {
+		      spawn: false,
+		    },
+		  },
 		},
 
 		// configurar daqui pra baixo
@@ -20,11 +28,20 @@ module.exports = function (grunt) {
 		      'css/style.css': ['src/styl/main.styl'] // compile and concat into single file
 		    },
 		  },
-		}
+		},
+
+		uglify: {
+	    my_target: {
+	      files: {
+	        'js/functions.min.js': ['src/js/functions.js', 'src/js/analytics.js']
+	      }
+	    }
+	  }
 
 	});
 
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-stylus');
-	grunt.registerTask('default', ['watch', 'stylus']);
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+	grunt.registerTask('default', ['watch', 'stylus', 'uglify']);
 };
